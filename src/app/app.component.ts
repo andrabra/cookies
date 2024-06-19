d5aa751edbfa46d9831ffeb4d1ae3436e5f1bf90
Andrew Abramov <andrabra@Andrews-MacBook-Pro.local>
2024-06-19 17:27:54 +0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { productsData } from './data/productsData';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cookies';
+  currency = '$';
+  productsData = productsData;
+
+  form = this.fb.group({
+    product: ['', Validators.required],
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  scrollTo(target: HTMLElement) {
+    target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
+  changeCurrency() {
+    let newCurrency = '$';
+    let coefficient = 1;
+    if (this.currency === '$') {
+      newCurrency = '₽';
+      coefficient = 90;
+    } else if (this.currency === '₽') {
+      newCurrency = 'BYN';
+      coefficient = 3;
+    } else if (this.currency === 'BYN') {
+      newCurrency = '€';
+      coefficient = 0.9;
+    } else if (this.currency === '€') {
+      newCurrency = '¥';
+      coefficient = 6.9;
+    }
+    this.currency = newCurrency;
+
+    this.productsData.forEach((product: any) => {
+      // product.price = Math.round(product.basePrice * coefficient);
+      product.price = +(product.basePrice * coefficient).toFixed(2);
+    });
+  }
 }
